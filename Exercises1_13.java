06a493afae2cfb49b0ec869d0556ec7482bfbee2
caseynlambert <54276213+caseynlambert@users.noreply.github.com>
2019-08-20 09:50:12 -0600

    Add files via upload

---
+++ Exercises1_13.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercises;
+
+/**
+ *
+ * @author Casey Lambert
+ * Date 08/20/2019
+ */
+// a = 3.4
+// b = 50.2
+// c = 2.1
+// d = .55
+// e = 44.5
+// f = 5.9
+public class Exercises1_13 {
+    public static void main(String[] args) {
+        System.out.println(((44.5 * .55) - (50.2 * 5.9)) / ((3.4 * .55) - (50.2 * 2.1)));
+        System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * .55) - (50.2 * 2.1)));
+        
+    }
+}
