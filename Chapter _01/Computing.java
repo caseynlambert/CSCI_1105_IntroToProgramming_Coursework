77668c1e1c24d95c56d769f1312ce5d3b81708e0
caseynlambert <54276213+caseynlambert@users.noreply.github.com>
2019-08-22 07:36:21 -0600

    Add files via upload

---
+++ Chapter _01/Computing.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Casey Lambert
+ * Date 08/20/2019
+ */
+public class Computing {
+    public static void main(String[] args) {
+      System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+    }
+}
